645cfc4fffd62092deefd98ea407f1a68d82f3be
sameermd16 <sameerthemc@gmail.com>
2023-07-04 23:04:24 +0530

    crypto

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 
 //unsplash api  
 //* URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
+//API for cryptocurrency
 
-fetch(" URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+const cryptoTopDiv = document.getElementById('crypto-top')
+const priceDivEl = document.getElementById('price')
+
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {                 //returning an object with key:value pairs 
+        // console.log(data)
+        // console.log(data.urls.regular)
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        document.getElementById('author').textContent = `Photo by: ${data.user.name}`
+        // throw Error('im an error')
+    }).catch(() => {
+        document.body.style.backgroundImage = `url(desert.jpg)`
+        //report the error to some kind of service
+    })
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(response => {
+        // console.log(response)
+        // console.log(response.ok)
+        if(!response.ok) {
+            throw Error("Something went wrong")
+        }
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+        cryptoTopDiv.innerHTML = `
+                                <img src="${data.image.small}"/>  
+                                <span>${data.name}</span>
+                                `
+        priceDivEl.innerHTML = `
+            <p>🎯:&#8377; ${data.market_data.current_price.inr}</p>
+            <p>👆:&#8377; ${data.market_data.high_24h.inr}</p>
+            <p>👇:&#8377; ${data.market_data.low_24h.inr}</p>
+        `                
+    })
+    .catch(err => console.error(err))
